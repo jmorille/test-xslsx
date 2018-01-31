7f84dcdc08d9b62efd2eb10b544bb8a74ba4f2d1
jmorille <jmorille@gmail.com>
2018-01-31 14:25:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 const fs = require('fs')
 const XLSX = require('xlsx');
+ 
 
+function addSheet(wb){
+   
+    var ws_name = "SheetJS";
 
-// https://github.com/SheetJS/js-xlsx/blob/master/tests/write.js
-var wb = XLSX.utils.book_new();
+    /* make worksheet */
+    var ws_data = [
+        [ "S", "h", "e", "e", "t", "J", "S" ],
+        [  1 ,  2 ,  3 ,  4 ,  5 ],
+        [  1 ,  2 ,  3 ,  4 ,  5 ]
+    ];
+    const ws = XLSX.utils.aoa_to_sheet(ws_data);
 
-var ws_name = "SheetJS";
+    /* Add the sheet name to the list */
 
-/* make worksheet */
-var ws_data = [
-    [ "S", "h", "e", "e", "t", "J", "S" ],
-    [  1 ,  2 ,  3 ,  4 ,  5 ],
-    [  1 ,  2 ,  3 ,  4 ,  5 ]
-];
-const ws = XLSX.utils.aoa_to_sheet(ws_data);
+    XLSX.utils.book_append_sheet(wb, ws, ws_name); 
+}    
 
-/* Add the sheet name to the list */
+function  main() {
+  // https://github.com/SheetJS/js-xlsx/blob/master/tests/write.js
+  var wb = XLSX.utils.book_new();
 
-XLSX.utils.book_append_sheet(wb, ws, ws_name);
-
-
-ws['C1'].f = "A1+B1";
-ws['C2'] = {t:'n', f:"A1+B1"};
-
-XLSX.writeFile(wb, 'out.xlsb');
+  XLSX.writeFile(wb, 'out.xlsb');
+) 
